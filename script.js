4687fcf78ce726ba854203fc05b10fc0d668fee7
shelialynn304 <shelialynn304@gmail.com>
2026-03-07 12:31:09 -0500

    Update script.js

--- script.js
+++ script.js
@@ -20,10 +20,6 @@ function setPreset(game) {
 
 }
 
-function setPreset(edge) {
-  document.getElementById("houseEdge").value = edge;
-}
-
 function clamp(value, min, max) {
   return Math.min(Math.max(value, min), max);
 }
